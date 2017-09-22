7b6edefe470a9b3a2160388377c0f59cf840f599
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2017-09-22 12:31:25 +0300

    Update deploy.sh

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -4,15 +4,52 @@
 
 WGET=$(which wget);
 
+source /etc/jelastic/environment
+
+function ensureFileCanBeDownloaded(){
+    local resource_url=$1;
+    resource_data_dize=$($CURL -s --head $resource_url | $GREP "Content-Length" | $AWK -F ":" '{ print $2 }'| $SED 's/[^0-9]//g');
+    freebytesleft=$(( 1024 *  $(df  | $GREP "/$" | $AWK '{ print $4 }' | head -n 1)-1024*1024));
+    [ -z ${resource_data_dize} ] && return 0;
+    [ ${resource_data_dize} -lt  ${freebytesleft} ] || { writeJSONResponseErr "result=>4075" "message=>No free diskspace"; die -q; }
+    return 0;
+}
+
+function getPackageName() {
+    if [ -f "$package_url" ]; then
+        package_name="$package_url";
+    elif [[ "${package_url}" =~ file://* ]]; then
+        package_name="${package_url:7}"
+        [ -f "$package_name" ] || { writeJSONResponseErr "result=>4078" "message=>Error loading file from URL"; die -q; }
+    else
+        ensureFileCanBeDownloaded $package_url;
+        $WGET --no-check-certificate --content-disposition --directory-prefix="$download_dir" $package_url >> $ACTIONS_LOG 2>&1 || { writeJSONResponseErr "result=>4078" "message=>Error loading file from URL"; die -q; }
+        package_name="${download_dir}/$(ls ${download_dir})";
+        [ ! -s "$package_name" ] && {
+            set -f
+            rm -f "${package_name}";
+            set +f
+            writeJSONResponseErr "result=>4078" "message=>Error loading file from URL";
+            die -q;
+        }
+    fi
+}
+
 function _deploy(){
-     [ "x${context}" == "xROOT" ] && context="root";
-     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war";
-     $WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
-     service cartridge restart 2>>/dev/null 1>>/dev/null;
+     [ "x${context}" == "xroot" ] && context="ROOT";
+     [ -f "${WEBROOT}/${context}.war" ] && rm -f ${WEBROOT}/${context}.war;
+     [ -f "${WEBROOT}/${context}.ear" ] && rm -f ${WEBROOT}/${context}.ear;
+     download_dir=$(mktemp -d)
+     getPackageName
+     set +f;
+     cp -f "${package_name}" ${WEBROOT}/"${context}.war"
+     chown -R jelastic:jelastic "${WEBROOT}"
+     rm -rf ${download_dir}
+     set -f;
 }
 
 function _undeploy(){
-     [ "x${context}" == "xROOT" ] && context="root";
-     [ -f "${WEBROOT}/${context}.war" ] && rm -f "${WEBROOT}/${context}.war";
+     [ "x${context}" == "xroot" ] && context="ROOT";
+     rm -rf "${WEBROOT}/${context}.war" "${WEBROOT}/${context}.war.*" 1>/dev/null;
+     rm -rf "${WEBROOT}/${context}.ear" "${WEBROOT}/${context}.ear.*" 1>/dev/null;
 }
-
